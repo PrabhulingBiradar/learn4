04969c2c9d0ba63be8c6d867a7e308d45ca2b922
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 10:51:07 +0530

    learning charjava

---
+++ charjava.java
@@ -0,0 +1,22 @@
+class charjava{
+public static void main(String[]args){
+
+
+
+char ch[]={'$','a','b','c','d','1','2','3','4','5'};
+
+System.out.println(ch[0]);
+System.out.println(ch[1]);
+System.out.println(ch[2]);
+System.out.println(ch[3]);
+System.out.println(ch[4]);
+System.out.println(ch[5]);
+System.out.println(ch[6]);
+System.out.println(ch[7]);
+System.out.println(ch[8]);
+System.out.println(ch[9]);
+
+
+
+}
+}
